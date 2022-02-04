8d8e49a77296a539b3bb8e56a398bab3936c91bd
AmadoCab <28881001+AmadoCab@users.noreply.github.com>
2022-02-04 00:02:44 -0600

    autocommit

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ def upload_drive(user,file_route,folder_id):
     drive_file.SetContentFile(file_route)
     drive_file.Upload()
 
+def update_drive(user,file_route,file_id):
+    credentials = user
+    drive_file = credentials.CreateFile({'id':file_id})
+    drive_file['title'] = file_route.split('/')[-1]
+    drive_file.SetContentFile(file_route)
+    drive_file.Upload()
+
+
 # Jinja2 #
 file_loader = FileSystemLoader('.')
 env = Environment(loader=file_loader,
@@ -26,6 +34,10 @@ template = env.get_template('main.tex')
 # Pydrive2 #
 credentials_route = 'credentials_module.json'
 folder_lab = '1gupZRyrHfVZK9h7RZ46ZqkEzjLRbkp4e'
+files_ids = {'astro':'1ingfjfTnBjrSVwXtB0gRXQEGm71L334L',
+    'epidemiologia':'1LBXfpps-A0XCS7QIammQsMQwXD5Y4-x2',
+    'machinelearning':'1R_RGpTTX4eByqvQhm5rQTFkSeDIp5oBw',
+    'profesores':'1pOzvkp5mTwPCMpYSQ4OaX9H2HYQ_qcJi'}
 
 gauth = GoogleAuth()
 gauth.LoadCredentialsFile(credentials_route)
@@ -44,7 +56,8 @@ for var in ['astro','epidemiologia','machinelearning','profesores']:
         tex_file.write(output)
     run(f'pdflatex -shell-escape {tex_name}',shell=True)
     run(f'mv {var}_complete.pdf output/', shell=True)
-    upload_drive(amado, f'output/{var}_complete.pdf', folder_lab)
+    #upload_drive(amado, f'output/{var}_complete.pdf', folder_lab)
+    update_drive(amado, f'output/{var}_complete.pdf', files_ids[var])
     run(f'rm {tex_name}',shell=True)
 run('rm *.log', shell=True)
 run('rm *.aux', shell=True)
